fa4e9ac125d4192e88746353b154d08f833a578a
lampo100 <lampo100@gmail.com>
2019-01-07 01:25:44 +0100

    Add abstract Chromosome nad its implementation to our problem

--- optonet/chromosome.py
+++ optonet/chromosome.py
@@ -1,8 +1,36 @@
-class Chromosome:
-    '''
-    DemandSolution holds information about possible solution to some given data demand
-    '''
-    def __init__(self, genes, fitness):
-        self.Genes = genes
-        self.Fitness = fitness
-        self.Age = 0
+from abc import ABC, abstractmethod
+
+class Chromosome(ABC):
+    """
+    Chromosome represents generic chromosome used in evolutionary algorithm
+    """
+
+
+class OptonetGene:
+    """
+    OptonetGene represents solution to one demand in optical network
+    """
+    def __init__(self, demand, paths, chosen_path, chosen_card):
+        """
+        Return initialized instance of OptonetGene
+        :param demand: value of the data demand
+        :param paths: all paths that can be used to satisfy this demand
+        :param chosen_path: path chosen to use for this demand
+        :param chosen_card: chosen transponder card to use for this demand
+        """
+        self.demand = demand
+        self.paths = paths
+        self.chosen_path = chosen_path
+        self.chosen_card = chosen_card
+
+
+class OptonetChromosome(Chromosome):
+    """
+    OptonetChromosome represents chromosome encoding solution to problem of finding optimal data flow in optical network
+    """
+    def __init__(self, genes):
+        """
+        Each gene includes solution to one demand
+        :param genes:
+        """
+        self.__genes = genes
